7a5d98efb78418a92bdaa80cec00be2237d2c27a
Ritvik Dayal <ritvik@sudofire.com>
2022-12-13 20:10:28 +0530

    added django project

---
+++ sample_app/models.py
@@ -0,0 +1,21 @@
+"""
+Design a Database schema for a Netflix like application.
+Considering the following entities:
+1. User
+2. Movie
+3. Genre
+4. Language
+
+Assumptions:
+1. A user can watch many movies.
+2. A movie can be watched by many users.
+3. A movie can have many genres.
+4. A genre can have many movies.
+
+Bonus:
+1. A user can have multiple profiles.
+"""
+
+from django.db import models
+
+# Create your models here.
---
+++ sample_app/views.py
@@ -0,0 +1,15 @@
+"""
+Design the api(s) for the following use cases:
+
+
+1. List of all movies available in the database.
+2. List of movies of a particular genre.
+3. List of movies that have been watched by a particular user.
+4. List all the genres of given movie.
+
+5. Mark a movie as watched by a user.
+6. Add a new movie to the database.
+
+Bonus:
+1. List all the movies that have been watched by all the users.
+"""
